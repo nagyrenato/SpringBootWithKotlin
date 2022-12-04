0572e6ad75b0d0ea6e200663ebed1e7e84b0d9af
Renato Nagy <renato_nagy@epam.com>
2022-12-04 17:24:18 +0100

    WIP fixing contextloads test.

--- src/test/kotlin/reno/learn/kotlin/KotlinExampleApplicationTests.kt
+++ src/test/kotlin/reno/learn/kotlin/KotlinExampleApplicationTests.kt
@@ -1,15 +1,21 @@
 package reno.learn.kotlin
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.junit4.SpringRunner
 
+@RunWith(SpringRunner::class)
 @SpringBootTest
 class KotlinExampleApplicationTests {
 
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
     @Test
-    fun contextLoads(context: ApplicationContext?) {
-        assertThat(context).isNotNull
+    fun contextLoads() {
+        println(applicationContext.displayName)
     }
 }
